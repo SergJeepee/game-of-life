bc3d44f46b2ab96ab50bc6c5b42b36d6afdf07b1
schashchukhin <schashchukhin@provectus.com>
2023-06-05 11:34:08 +0300

    rename executables

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 rm -rf bin/
 GOOS=darwin GOARCH=arm64 go build -o bin/GameOfLife-mac-arm main.go
-GOOS=darwin GOARCH=amd64 go build -o bin/GameOfLife-mac-int main.go
-GOOS=windows GOARCH=amd64 go build -o bin/GameOfLife-win main.go
-GOOS=linux GOARCH=amd64 go build -o bin/GameOfLife-linux main.go
+GOOS=darwin GOARCH=amd64 go build -o bin/GameOfLife-mac-x64 main.go
+GOOS=windows GOARCH=amd64 go build -o bin/GameOfLife-win-x64 main.go
+GOOS=linux GOARCH=amd64 go build -o bin/GameOfLife-linux-x64 main.go
